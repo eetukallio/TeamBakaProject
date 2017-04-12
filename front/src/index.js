d8911f31248bba2e9f730cb5e79087c05b9bd015
eetukallio <eetu.kallio@gmail.com>
2017-04-12 19:31:55 +0300

    Modify routing structure

--- front/src/index.js
+++ front/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Login from './views/login/Login'
-import Paychecks from './views/info/Info';
-import HourEntry from './views/shoppingCart/ShoppingCart';
-import Management from './views/browse/Browse';
+import Info from './views/info/Info';
+import ShoppingCart from './views/shoppingCart/ShoppingCart';
+import Browse from './views/browse/Browse';
 import thunk from 'redux-thunk';
 import { Router, Route, IndexRoute } from 'react-router';
 import { Provider } from 'react-redux';
@@ -15,9 +15,6 @@ import { homeReducer } from './reducers/index';
 import { checkAuth } from './utils/checkAuth';
 import cookie from 'react-cookie';
 import {SET_AUTH} from './constants/AppConstants';
-import Customers from './views/browse/browseComponents/Customers';
-import Workers from './views/browse/browseComponents/Products';
-import HourEntries from './views/browse/browseComponents/HourEntries';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const store = createStoreWithMiddleware(homeReducer);
@@ -32,12 +29,11 @@ ReactDOM.render(
     <Provider store={store}>
         <Router history={browserHistory}>
             <Route path="/" component={App}>
-                <IndexRoute component={Login}/>
-                <Route path="/login" component={Login}/>
-                <Route path="/home" component={checkAuth(HourEntry)}/>
-                <Route path="/info" component={checkAuth(Paychecks)}/>
-                <Route path="/browse" component={checkAuth(Management)}>
-                </Route>
+                <IndexRoute component={Browse}/>
+                {/*<Route path="/login" component={Login}/>*/}
+                <Route path="/cart" component={ShoppingCart}/>
+                <Route path="/info" component={Info}/>
+                <Route path="/browse" component={Browse}/>
             </Route>
         </Router>
     </Provider>,
