eec489b594be20bbfec64f0ceb635a7fc272d442
eetukallio <eetu.kallio@gmail.com>
2017-04-12 21:24:24 +0300

    Add dummydata flooding to backend

--- server/src/main/java/fi/tamk/tiko/ProductController.java
+++ server/src/main/java/fi/tamk/tiko/ProductController.java
@@ -13,19 +13,32 @@ public class ProductController {
     ProductRepository db;
 
     @RequestMapping(value = "/products",  method=RequestMethod.POST)
-    public void saveLocation(@RequestBody Product ml) {
+    public void saveProduct(@RequestBody Product ml) {
         db.save(ml);
+    }
+
+    @RequestMapping(value = "/products/flood",  method=RequestMethod.POST)
+    public void floodProducts() {
+
+        String[] names = {"Onodera", "Chitoge", "Subaru", "Hanekawa", "Senjougahara"};
+        int random;
+
+        for (int i = 0; i < 100; i++) {
+            random = (int) Math.floor((Math.random()*5));
+            db.save(new Product(i, i, names[random] + " best girl", i + "cm x " + i + "cm x " + i +"cm",
+                    "myanimelist.com"));
+        }
     }
 
     @RequestMapping(value = "/products", method = RequestMethod.GET)
     // The return value will be the HTTP Body
     @ResponseBody
-    public Iterable<Product> fetchLocations() {
+    public Iterable<Product> fetchProducts() {
         return db.findAll();
     }
 
     @RequestMapping(value = "/products/{productId}",  method=RequestMethod.GET)
-    public Product fetchLocation(@PathVariable long locationId) {
+    public Product fetchProduct(@PathVariable long locationId) {
         return db.findOne(locationId);
     }
 }
