fe5c9ecc22f00c9603baef9a496c55509b606b96
Eetu Kallio <eetu.kallio@gmail.com>
2017-05-23 16:43:37 +0300

    Add some start-up information prints to backend

--- server/src/main/java/fi/tamk/tiko/Application.java
+++ server/src/main/java/fi/tamk/tiko/Application.java
@@ -18,6 +18,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
+        System.out.println();
+        System.out.println("WELCOME TO BODY PILLOW E-STORE BACKEND" +
+                "\n" +
+                "The frontpage of this application is available at 'http://localhost:8080'" +
+                "\n" +
+                "from there on you may navigate by using the links provided by the site or use the following URLs:" +
+                "\n" +
+                "'http://localhost:8080/browse' \n" +
+                "'http://localhost:8080/item?id={id}'\n" +
+                "'http://localhost:8080/browse?search={search}'\n" +
+                "'http://localhost:8080/orders'\n" +
+                "'http://localhost:8080/config'\n" +
+                "'http://localhost:8080/cart'\n" +
+                "'http://localhost:8080/login'\n");
     }
 }
