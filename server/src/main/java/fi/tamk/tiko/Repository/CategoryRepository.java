72b313a806132fb777334771a56d5bc97657408f
Eetu Kallio <eetu.kallio@gmail.com>
2017-05-25 22:35:31 +0300

    Add even more documentation to the backend

--- server/src/main/java/fi/tamk/tiko/Repository/CategoryRepository.java
+++ server/src/main/java/fi/tamk/tiko/Repository/CategoryRepository.java
@@ -6,14 +6,12 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 /**
- * Created by Eetu Kallio on 20.5.2017
+ * Category repository
+ *
+ * @author Eetu Kallio
+ * @since 3.0
+ * @version 3.0
  */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
-
-    public List<Category> findAll();
-
-    public List<Category> findAllById(Long id);
-
-    public void deleteById(Long id);
 }
